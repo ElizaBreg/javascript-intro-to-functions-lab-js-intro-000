66dcef641b5d667a90359de7eac2df461215a934
Elizabeth <elizbregman@gmail.com>
2020-07-05 18:21:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
   return "I can't hear you!"
 }else if (string === string.toUpperCase()) {
-  console.log("YES INDEED!")
+  return "YES INDEED!"
 } else if (string === "I love you, Grandma.") {
-  console.log("I love you, too.")
+  return "I love you, too."
 }
 }
