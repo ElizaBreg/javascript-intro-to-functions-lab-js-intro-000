047e7a8dbcea0e5323ab3cc9e584cfd2e5b1adef
Elizabeth <elizbregman@gmail.com>
2020-07-05 17:57:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  console.log("I can\'t hear you!")
+  console.log('I can\'t hear you!')
 }
